fc3a4df983cdd0204ca9baf6fa478e28860fb08d
KurnakovMaksim <maksk210@gmail.com>
2022-04-23 15:41:16 +0900

    #2 Add LabelEntity

---
+++ Goal/src/jiraF.Goal.API/Infrastructure/Data/Entities/LabelEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jiraF.Goal.API.Infrastructure.Data.Entities;
+
+[Table("label")]
+public class LabelEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id_label")]
+    public Guid Id { get; set; }
+
+    [Column("l_title")]
+    public string Title { get; set; }
+}
