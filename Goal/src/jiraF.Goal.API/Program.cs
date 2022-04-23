76e80b83f8e07b7033e277c2a0a6f6e3f14f7cbc
KurnakovMaksim <maksk210@gmail.com>
2022-04-23 17:28:06 +0900

    #7 Connect db

--- Goal/src/jiraF.Goal.API/Program.cs
+++ Goal/src/jiraF.Goal.API/Program.cs
@@ -1,11 +1,23 @@
+using jiraF.Goal.API.Contracts;
+using jiraF.Goal.API.Infrastructure.Data.Contexts;
+using jiraF.Goal.API.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IGoalRepository, GoalRepository>();
+builder.Services.AddTransient<ILabelRepository, LabelRepository>();
 
 var app = builder.Build();
 
