49a6705e92a1ce1f5f20c689b9f0a93d0c47c2da
KurnakovMaksim <maksk210@gmail.com>
2022-04-23 16:24:48 +0900

    #5 Add Title value object

---
+++ Goal/src/jiraF.Goal.API/ValueObjects/Title.cs
@@ -0,0 +1,18 @@
+﻿using jiraF.Goal.API.Exceptions;
+
+namespace jiraF.Goal.API.ValueObjects;
+
+public class Title
+{
+    public string Value { get; }
+
+    public Title(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ValueObjectException("Cannot set empty value.");
+        if (value.Length <= 1 || value.Length >= 51)
+            throw new ValueObjectException("Invalid length (availability 2 - 50)");
+
+        Value = value;
+    }
+}
---
+++ Goal/src/jiraF.Goal.UnitTests/ValueObjects/TitleTests.cs
@@ -0,0 +1,42 @@
+﻿using jiraF.Goal.API.Exceptions;
+using jiraF.Goal.API.ValueObjects;
+using Xunit;
+
+namespace jiraF.Goal.UnitTests.ValueObjects;
+
+public class TitleTests
+{
+    private const string VALUE_GREATER_THAN_50_CHARACTERS = "123456789112345678921234567893123456789412345678951";
+
+    [Fact]
+    public void Ctor_CanCreateValidTitle_Title()
+    {
+        // Arrange
+        string validValue = "Test title value";
+
+        // Act
+        Title result = new(validValue);
+
+        // Assert
+        Assert.Equal(validValue, result.Value);
+    }
+
+    [Fact]
+    public void Ctor_CannotCreateValidTitleIfValueIsSpace_ValueObjectException()
+    {
+        // Arrange
+        string invalidValue = " ";
+
+        // Act, Assert
+        Assert.Throws<ValueObjectException>(() => new Title(invalidValue));
+    }
+
+    [Theory]
+    [InlineData("1")]
+    [InlineData(VALUE_GREATER_THAN_50_CHARACTERS)]
+    public void Ctor_CannotCreateValidTitleIfLengthIsInvalie_ValueObjectException(string invalidValue)
+    {
+        // Act, Assert
+        Assert.Throws<ValueObjectException>(() => new Title(invalidValue));
+    }
+}
