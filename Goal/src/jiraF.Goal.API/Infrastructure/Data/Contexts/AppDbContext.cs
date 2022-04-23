7555372c004b58599e92bf2f56a58277c7157ea2
KurnakovMaksim <maksk210@gmail.com>
2022-04-23 15:49:36 +0900

    #1 Add GoalEntity

---
+++ Goal/src/jiraF.Goal.API/Infrastructure/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using jiraF.Goal.API.Infrastructure.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace jiraF.Goal.API.Infrastructure.Data.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<GoalEntity> Goals { get; set; }
+    public DbSet<LabelEntity> Labels { get; set; }
+}
---
+++ Goal/src/jiraF.Goal.API/Infrastructure/Data/Entities/GoalEntity.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jiraF.Goal.API.Infrastructure.Data.Entities;
+
+[Table("goal")]
+public class GoalEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id_goal")]
+    public Guid Id { get; set; }
+
+    [Column("g_title")]
+    public string Title { get; set; }
+
+    [Column("g_description")]
+    public string Description { get; set; }
+
+    [Column("g_reporter_id")]
+    public Guid ReporterId { get; set; }
+
+    [Column("g_assignee_id")]
+    public Guid AssigneeId { get; set; }
+
+    [Column("g_date_of_create")]
+    public DateTime DateOfCreate { get; set; }
+
+    [Column("g_date_of_update")]
+    public DateTime DateOfUpdate { get; set; }
+
+    [Column("g_label_id")]
+    public Guid LabelId { get; set; }
+}
