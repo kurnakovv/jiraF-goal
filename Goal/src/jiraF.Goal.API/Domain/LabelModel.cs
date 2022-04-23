03d859bfa3ad3634ee019c983fb3d27e7ccb7903
KurnakovMaksim <maksk210@gmail.com>
2022-04-23 16:45:58 +0900

    #9 Add LabelModel

---
+++ Goal/src/jiraF.Goal.API/Domain/LabelModel.cs
@@ -0,0 +1,14 @@
+﻿using jiraF.Goal.API.ValueObjects;
+
+namespace jiraF.Goal.API.Domain;
+
+public class LabelModel
+{
+    public Title Title { get; set; }
+
+    public LabelModel(
+        Title title)
+    {
+        Title = title;
+    }
+}
