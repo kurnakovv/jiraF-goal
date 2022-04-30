d0801b1fc8cdbf3db48275084f78809b44c3cef8
KurnakovMaksim <maksk210@gmail.com>
2022-04-30 12:58:34 +0900

    #10 Add GoalController.Get

---
+++ Goal/src/jiraF.Goal.API/Controllers/GoalController.cs
@@ -0,0 +1,24 @@
+﻿using jiraF.Goal.API.Contracts;
+using jiraF.Goal.API.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jiraF.Goal.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GoalController : ControllerBase
+{
+    private readonly IGoalRepository _goalRepository;
+
+    public GoalController(
+        IGoalRepository goalRepository)
+    {
+        _goalRepository = goalRepository;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<GoalModel>> Get()
+    {
+        return await _goalRepository.GetAsync();
+    }
+}
---
+++ Goal/src/jiraF.Goal.EndToEndTests/Controllers/GoalController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace jiraF.Goal.EndToEndTests.Controllers
+{
+    public class GoalController
+    {
+        private readonly HttpClient _client;
+
+        public GoalController()
+        {
+            var application = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    // ... Configure test services
+                });
+
+            _client = application.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("/Goal")]
+        public async Task CheckAllApiMethodsIsValid_StatusCode200(string url)
+        {
+            HttpResponseMessage response = await _client.GetAsync(url);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8",
+                response?.Content?.Headers?.ContentType?.ToString());
+        }
+    }
+}
