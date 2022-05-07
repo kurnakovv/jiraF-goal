4cf56870baa2c4fee25c0f7bdd48d666b943e850
KurnakovMaksim <maksk210@gmail.com>
2022-05-07 12:58:56 +0900

    #10 Add test data for Goal

--- Goal/src/jiraF.Goal.API/Infrastructure/Data/Contexts/AppDbContext.cs
+++ Goal/src/jiraF.Goal.API/Infrastructure/Data/Contexts/AppDbContext.cs
@@ -5,7 +5,12 @@ namespace jiraF.Goal.API.Infrastructure.Data.Contexts;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+    {
+#if DEBUG
+        new TestData(this).Seed();
+#endif
+    }
 
     public DbSet<GoalEntity> Goals { get; set; }
     public DbSet<LabelEntity> Labels { get; set; }
