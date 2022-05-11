7a01ed256e6347a9aed63062e087fa293b06f360
KurnakovMaksim <maksk210@gmail.com>
2022-05-11 12:15:28 +0900

    #11 Add LabelController.Get

---
+++ Goal/src/jiraF.Goal.API/Controllers/LabelController.cs
@@ -0,0 +1,24 @@
+﻿using jiraF.Goal.API.Contracts;
+using jiraF.Goal.API.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jiraF.Goal.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LabelController : ControllerBase
+{
+    private readonly ILabelRepository _labelRepository;
+
+    public LabelController(
+        ILabelRepository labelRepository)
+    {
+        _labelRepository = labelRepository;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<LabelModel>> Get()
+    {
+        return await _labelRepository.GetAsync();
+    }
+}
