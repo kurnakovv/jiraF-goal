72cb208343f2b3ed7cd9406b31457f855497d181
KurnakovMaksim <maksk210@gmail.com>
2022-05-21 12:37:01 +0900

    #72 Add goal component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Goal from './components/Goal';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Goal 
+        key={'test1'}
+        id='test1'
+        title='title1'
+        description='desc1'
+      />
+      <Goal 
+        key={'test2'}
+        id='test2'
+        title='title2'
+        description='desc2'
+      />
+      <Goal 
+        key={'test3'}
+        id='test3'
+        title='title3'
+        description='desc3'
+      />
     </div>
   );
 }
---
+++ client/src/components/Goal/IGoal.ts
@@ -0,0 +1,5 @@
+export interface IGoal {
+    id: string,
+    title: string,
+    description: string
+}
