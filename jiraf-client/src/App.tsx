0cb5324497dc97a06621ef6a3604ec6a7a56c5dc
KurnakovMaksim <maksk210@gmail.com>
2022-05-21 20:42:03 +0900

    #72 Connect goals

--- jiraf-client/src/App.tsx
+++ jiraf-client/src/App.tsx
@@ -2,28 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Goal from './components/Goal';
+import axios from 'axios';
+import { IGoal } from './components/Goal/IGoal';
 
 function App() {
+  const [goals, setGoals] = React.useState<IGoal[]>([]);
+
+  React.useEffect(() => {
+    axios.get("https://jiraf-goal.herokuapp.com/goal", {
+      headers: {
+        "GoalApiKey": `${process.env.REACT_APP_GOAL_API_KEY}`,
+      }
+    }).then(({ data }) => {
+      setGoals(data.goals);
+    });
+  }, [])
+
   return (
     <div className="App">
-      <Goal 
-        key={'test1'}
-        id='test1'
-        title='title1'
-        description='desc1'
-      />
-      <Goal 
-        key={'test2'}
-        id='test2'
-        title='title2'
-        description='desc2'
-      />
-      <Goal 
-        key={'test3'}
-        id='test3'
-        title='title3'
-        description='desc3'
-      />
+      {
+        goals && goals.map((goal: IGoal) => {
+          return (
+            <Goal
+              key={goal.id}
+              id={goal.id}
+              title={goal.title}
+              description={goal.description}
+            />
+          )
+        })
+      }
     </div>
   );
 }
