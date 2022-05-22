eca051ec15f30961da099a248956f459400cee0d
KurnakovMaksim <maksk210@gmail.com>
2022-05-22 16:38:16 +0900

    #94 Optimize add goal

--- jiraf-client/src/App.tsx
+++ jiraf-client/src/App.tsx
@@ -5,6 +5,7 @@ import Goal from './components/Goal';
 import axios from 'axios';
 import { IGoal } from './components/Goal/IGoal';
 import AddGoalPanel from './components/AddGoalPanel';
+import { IAddGoalRequestDto } from './components/AddGoalPanel/IAddGoalRequestDto';
 
 function App() {
   const [goals, setGoals] = React.useState<IGoal[]>([]);
@@ -20,17 +21,45 @@ function App() {
     });
   }, [])
 
-  React.useEffect(() => {
-    axios.get("https://jiraf-goal.herokuapp.com/goal", {
-      headers: {
-        "GoalApiKey": `${process.env.REACT_APP_GOAL_API_KEY}`,
-      }
-    }).then(({ data }) => {
-      setGoals(data.goals);
-    });
-  }, [!isAddedGoal])
+  const handleAdd = (e: any): void => {
+    e.preventDefault();
+    
+    // TODO: Try to fix any -> IAddGoalRequestDto
+    const goalDto: any = {
+        title: e.target.elements.title.value,
+        description: e.target.elements.description.value,
+        reporterId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        assigneeId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        labelTitle: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+    }
+
+    try{
+      setGoals((prev) => [...prev, goalDto])
+      axios.post("https://jiraf-goal.herokuapp.com/goal", goalDto, {
+        headers: {
+          "GoalApiKey": `${process.env.REACT_APP_GOAL_API_KEY}`,
+        }
+      }).then(({ data }) => {
+        setGoals((prev) => 
+        prev.map((goal: IGoal) => {
+          if (goal.id === undefined) {
+            return {
+              ...goal,
+              id: data.id,
+            }
+          }
+          return goal;
+      }));
+      });
+      e.target.reset();
+      setIsAddedGoal(true);
+    } catch (error) {
+      console.log(error);
+    }
+}
 
   const handleDeleteGoal = (id: string): void => {
+    setIsAddedGoal(false);
     try {
       axios.delete(`https://jiraf-goal.herokuapp.com/goal?id=${id}`, {
         headers: {
@@ -50,7 +79,7 @@ function App() {
         : <></>
       }
       <AddGoalPanel
-        setIsAddedGoal={setIsAddedGoal}
+        handleAdd={handleAdd}
       />
       {
         goals && goals.map((goal: IGoal) => {
--- jiraf-client/src/components/AddGoalPanel/index.tsx
+++ jiraf-client/src/components/AddGoalPanel/index.tsx
@@ -2,34 +2,10 @@ import axios from "axios";
 import { IAddGoalRequestDto } from "./IAddGoalRequestDto";
 
 const AddGoalPanel: React.FC<any> = (props) => {
-    const handleAdd = (e: any): void => {
-        e.preventDefault();
-        
-        const goalDto: IAddGoalRequestDto = {
-            title: e.target.elements.title.value,
-            description: e.target.elements.description.value,
-            reporterId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            assigneeId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            labelTitle: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-        }
-
-        try{
-            axios.post("https://jiraf-goal.herokuapp.com/goal", goalDto, {
-              headers: {
-                "GoalApiKey": `${process.env.REACT_APP_GOAL_API_KEY}`,
-              }
-            });
-            e.target.reset();
-            props.setIsAddedGoal(true);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     return (
         <>
             <div className="Container">
-                <form onSubmit={handleAdd}>
+                <form onSubmit={props.handleAdd}>
                     <div className="addSection">
                         <p className="subTitle">Title</p>
                         <input name="title" placeholder="enter your title" />
