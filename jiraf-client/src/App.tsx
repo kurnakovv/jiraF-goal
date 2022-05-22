4f63a1b7dacd90c92c277ffe1e0511ea4b7b214c
KurnakovMaksim <maksk210@gmail.com>
2022-05-22 15:14:32 +0900

    #89 Add goal for SPA

--- jiraf-client/src/App.tsx
+++ jiraf-client/src/App.tsx
@@ -4,9 +4,11 @@ import './App.css';
 import Goal from './components/Goal';
 import axios from 'axios';
 import { IGoal } from './components/Goal/IGoal';
+import AddGoalPanel from './components/AddGoalPanel';
 
 function App() {
   const [goals, setGoals] = React.useState<IGoal[]>([]);
+  const [isAddedGoal, setIsAddedGoal] = React.useState<boolean>(false);
 
   React.useEffect(() => {
     axios.get("https://jiraf-goal.herokuapp.com/goal", {
@@ -18,8 +20,25 @@ function App() {
     });
   }, [])
 
+  React.useEffect(() => {
+    axios.get("https://jiraf-goal.herokuapp.com/goal", {
+      headers: {
+        "GoalApiKey": `${process.env.REACT_APP_GOAL_API_KEY}`,
+      }
+    }).then(({ data }) => {
+      setGoals(data.goals);
+    });
+  }, [isAddedGoal])
+
   return (
     <div className="App">
+      {isAddedGoal 
+        ? <><h1>Added new goal!</h1><button onClick={() => setIsAddedGoal(false)}>X</button></>
+        : <></>
+      }
+      <AddGoalPanel
+        setIsAddedGoal={setIsAddedGoal}
+      />
       {
         goals && goals.map((goal: IGoal) => {
           return (
---
+++ jiraf-client/src/components/AddGoalPanel/IAddGoalRequestDto.ts
@@ -0,0 +1,7 @@
+export interface IAddGoalRequestDto {
+    title: string,
+    description: string,
+    reporterId: string,
+    assigneeId: string,
+    labelTitle: string
+}
