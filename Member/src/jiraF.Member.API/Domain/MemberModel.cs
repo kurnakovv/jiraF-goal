5aaf1a74ff049bbde575555c3677cea2fa7834d2
KurnakovMaksim <maksk210@gmail.com>
2022-06-11 12:15:34 +0900

    #98 Add member domain model

---
+++ Member/src/jiraF.Member.API/Domain/MemberModel.cs
@@ -0,0 +1,24 @@
+﻿namespace jiraF.Member.API.Domain;
+
+public class MemberModel
+{
+    public Guid Number { get; }
+    public DateTime DateOfCreate { get; }
+    public string Name { get; }
+
+    public MemberModel(
+        string name)
+    {
+        Name = name;
+    }
+
+    public MemberModel(
+        Guid number,
+        DateTime dateOfCreate,
+        string name)
+    {
+        Number = number;
+        DateOfCreate = dateOfCreate;
+        Name = name;
+    }
+}
