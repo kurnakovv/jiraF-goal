114607963e305c0403d502ca07848cf95da34cb3
KurnakovMaksim <maksk210@gmail.com>
2022-06-11 12:41:51 +0900

    #97 Add AppDbContext

---
+++ Member/src/jiraF.Member.API/Infrastructure/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using jiraF.Member.API.Infrastructure.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace jiraF.Member.API.Infrastructure.Data.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<MemberEntity> Members { get; set; }
+}
