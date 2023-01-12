807337cd1078d56979a2fd3bd44263f364731532
KurnakovMaksim <maksk210@gmail.com>
2023-01-12 22:20:52 +0900

    #38 Add base logic for exceptions

--- Goal/src/jiraF.Goal.API/Exceptions/DomainException.cs
+++ Goal/src/jiraF.Goal.API/Exceptions/DomainException.cs
@@ -1,7 +1,16 @@
-﻿namespace jiraF.Goal.API.Exceptions;
+﻿using System.Runtime.Serialization;
+
+namespace jiraF.Goal.API.Exceptions;
 
 [Serializable]
 public class DomainException : Exception
 {
     public DomainException(string message): base(message) { }
+
+    protected DomainException(SerializationInfo info, StreamingContext context) { }
+
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        base.GetObjectData(info, context);
+    }
 }
--- Goal/src/jiraF.Goal.API/Exceptions/ValueObjectException.cs
+++ Goal/src/jiraF.Goal.API/Exceptions/ValueObjectException.cs
@@ -1,7 +1,16 @@
-﻿namespace jiraF.Goal.API.Exceptions;
+﻿using System.Runtime.Serialization;
+
+namespace jiraF.Goal.API.Exceptions;
 
 [Serializable]
 public class ValueObjectException : Exception
 {
     public ValueObjectException(string message): base(message) { }
+
+    protected ValueObjectException(SerializationInfo info, StreamingContext context) { }
+
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        base.GetObjectData(info, context);
+    }
 }
