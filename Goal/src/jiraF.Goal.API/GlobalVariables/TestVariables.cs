2fca1dbf09b4ef1157fbe2577c599a966fdfc8ee
KurnakovMaksim <maksk210@gmail.com>
2023-01-18 22:40:05 +0900

    #63 Add getters and setters for TestVariables.IsWorkNow

--- Goal/src/jiraF.Goal.API/GlobalVariables/TestVariables.cs
+++ Goal/src/jiraF.Goal.API/GlobalVariables/TestVariables.cs
@@ -5,5 +5,5 @@ public static class TestVariables
     /// <summary>
     /// This variable turn on memory db with guid name if this variable = true.
     /// </summary>
-    public static bool IsWorkNow = false;
+    public static bool IsWorkNow { get; set; }
 }
