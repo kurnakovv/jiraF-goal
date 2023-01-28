e93becabe430e9e84385045ea3135fd413e99457
KurnakovMaksim <maksk210@gmail.com>
2023-01-28 17:35:02 +0900

    #218 Connect MemberApiClient to GoalController.Get

--- Goal/src/jiraF.Goal.API/Controllers/GoalController.cs
+++ Goal/src/jiraF.Goal.API/Controllers/GoalController.cs
@@ -9,6 +9,7 @@ using jiraF.Goal.API.Dtos.Goal.Get;
 using jiraF.Goal.API.Dtos.Goal.GetById;
 using jiraF.Goal.API.Dtos.Goal.Update;
 using jiraF.Goal.API.Dtos.Label;
+using jiraF.Goal.API.Infrastructure.ApiClients;
 using jiraF.Goal.API.ValueObjects;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
@@ -22,11 +23,15 @@ namespace jiraF.Goal.API.Controllers;
 public class GoalController : ControllerBase
 {
     private readonly IGoalRepository _goalRepository;
+    private readonly MemberApiClient _memberApiClient;
 
     public GoalController(
-        IGoalRepository goalRepository)
+        IGoalRepository goalRepository,
+        IHttpClientFactory httpClientFactory,
+        IConfiguration configuration)
     {
         _goalRepository = goalRepository;
+        _memberApiClient = new MemberApiClient(httpClientFactory.CreateClient(configuration.GetValue<string>("ApiClients:MemberApiClient")));
     }
 
     [HttpGet]
@@ -36,21 +41,7 @@ public class GoalController : ControllerBase
         List<Guid> memberIds = new(); 
         memberIds.AddRange(goals.Select(x => x.Reporter.Number));
         memberIds.AddRange(goals.Select(x => x.Assignee.Number));
-        IEnumerable<MemberDto> members = new List<MemberDto>();
-        using (HttpClient client = new() { BaseAddress = new Uri("https://jiraf-member.onrender.com") })
-        {
-            string jsonModel = JsonSerializer.Serialize(memberIds);
-            var stringContent = new StringContent(jsonModel, UnicodeEncoding.UTF8, "application/json");
-            HttpResponseMessage response = await client.PostAsync("/Member/GetByIds", stringContent);
-            if (response.IsSuccessStatusCode)
-            {
-                string json = await response.Content.ReadAsStringAsync();
-                members = JsonSerializer.Deserialize<IEnumerable<MemberDto>>(json, new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true
-                });
-            }
-        }
+        IEnumerable<MemberDto> members = await _memberApiClient.GetAsync(memberIds);
         IEnumerable<GoalModel> goalsWithMembers = 
             from g in goals
             join m in members on g.Reporter.Number equals m.Id into reporters
