540aff72e45c776812a816525174958d0a910b28
KurnakovMaksim <maksk210@gmail.com>
2023-02-22 20:45:03 +0900

    #232 Edit default member for MemberApiClient.IsExists test

--- Goal/src/jiraF.Goal.IntegrationTests/Infrastructure/ApiClients/MemberApiClientTests.cs
+++ Goal/src/jiraF.Goal.IntegrationTests/Infrastructure/ApiClients/MemberApiClientTests.cs
@@ -1,4 +1,5 @@
-﻿using jiraF.Goal.API.Infrastructure.ApiClients;
+﻿using jiraF.Goal.API.GlobalVariables;
+using jiraF.Goal.API.Infrastructure.ApiClients;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -17,6 +18,7 @@ public class MemberApiClientTests
         {
             BaseAddress = new Uri("https://jiraf-member.onrender.com/")
         });
+        DefaultMemberVariables.Id = "94ff67f3-294b-43f1-88ce-b815e80ff278";
     }
 
     [Fact]
@@ -41,7 +43,7 @@ public class MemberApiClientTests
     public async Task IsExistsAsync_MemberIsExists_PassWithoutException()
     {
         Exception exception = await Record.ExceptionAsync(async () =>
-            await _memberApiClient.IsExistsAsync(new Guid("2f857708-6e97-413b-b495-f2161135616a"))
+            await _memberApiClient.IsExistsAsync(new Guid(DefaultMemberVariables.Id))
         );
         Assert.Null(exception);
     }
