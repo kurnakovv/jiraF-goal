b05da708a04ee94388e3398c6a86aab4c1ce5170
kurnakovv <maksk210@gmail.com>
2024-01-28 18:03:42 +0900

    #334 Try to fix tests

--- Goal/src/jiraF.Goal.API/Infrastructure/Data/TestData.cs
+++ Goal/src/jiraF.Goal.API/Infrastructure/Data/TestData.cs
@@ -2,6 +2,7 @@
 using jiraF.Goal.API.Infrastructure.Data.Contexts;
 using jiraF.Goal.API.Infrastructure.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace jiraF.Goal.API.Infrastructure.Data
 {
@@ -19,43 +20,38 @@ namespace jiraF.Goal.API.Infrastructure.Data
 
         public void Seed()
         {
-            _dbContext.ChangeTracker.Clear();
-            if (!_dbContext.Goals.Any())
+            var goal1 = _dbContext.Goals.FirstOrDefault(x => x.Id == new Guid("a27723d9-fd4c-4b83-add8-f1c9152585ea"));
+            if (goal1 == null)
             {
-                if (!_dbContext.Goals.Any(x => x.Id == new Guid("a27723d9-fd4c-4b83-add8-f1c9152585ea")))
-                {
-                    _dbContext.Goals.Add(new GoalEntity() { Id = new Guid("a27723d9-fd4c-4b83-add8-f1c9152585ea"), AssigneeId = new Guid(DefaultMemberVariables.Id), ReporterId = new Guid(DefaultMemberVariables.Id), LabelId = _labelId, DateOfCreate = DateTime.UtcNow, Description = "Test desc1", Title = "Test title1" });
-                }
-                if (!_dbContext.Goals.Any(x => x.Id == new Guid("a27723d9-fd4c-4b83-add8-f1c9152585eb")))
-                {
-                    _dbContext.Goals.Add(new GoalEntity() { Id = new Guid("a27723d9-fd4c-4b83-add8-f1c9152585eb"), AssigneeId = new Guid(DefaultMemberVariables.Id), ReporterId = new Guid(DefaultMemberVariables.Id), LabelId = _labelId, DateOfCreate = DateTime.UtcNow, Description = "Test desc2", Title = "Test title2" });
-                }
-                if (!_dbContext.Goals.Any(x => x.Id == new Guid("a27723d9-fd4c-4b83-add8-f1c9152585ec")))
-                {
-                    _dbContext.Goals.Add(new GoalEntity() { Id = new Guid("a27723d9-fd4c-4b83-add8-f1c9152585ec"), AssigneeId = new Guid(DefaultMemberVariables.Id), ReporterId = new Guid(DefaultMemberVariables.Id), LabelId = _labelId, DateOfCreate = DateTime.UtcNow, Description = "Test desc3", Title = "Test title3" });
-                }
-                _dbContext.SaveChanges();
-                _dbContext.ChangeTracker.Clear();
+                _dbContext.Goals.Add(new GoalEntity() { Id = new Guid("a27723d9-fd4c-4b83-add8-f1c9152585ea"), AssigneeId = new Guid(DefaultMemberVariables.Id), ReporterId = new Guid(DefaultMemberVariables.Id), LabelId = _labelId, DateOfCreate = DateTime.UtcNow, Description = "Test desc1", Title = "Test title1" });
             }
-
-            if (!_dbContext.Labels.Any())
+            var goal2 = _dbContext.Goals.FirstOrDefault(x => x.Id == new Guid("a27723d9-fd4c-4b83-add8-f1c9152585eb"));
+            if (goal2 == null)
             {
-                if (!_dbContext.Labels.Any(x => x.Id == new Guid("4674f93c-6331-4e63-b298-349619fa8741")))
-                {
-                    _dbContext.Labels.Add(new LabelEntity() { Id = new Guid("4674f93c-6331-4e63-b298-349619fa8741"), Title = "Test title1" });
-                }
-                if (!_dbContext.Labels.Any(x => x.Id == new Guid("4674f93c-6331-4e63-b298-349619fa8742")))
-                {
-                    _dbContext.Labels.Add(new LabelEntity() { Id = new Guid("4674f93c-6331-4e63-b298-349619fa8742"), Title = "Test title2" });
-                }
-                if (!_dbContext.Labels.Any(x => x.Id == new Guid("4674f93c-6331-4e63-b298-349619fa8743")))
-                {
-                    _dbContext.Labels.Add(new LabelEntity() { Id = new Guid("4674f93c-6331-4e63-b298-349619fa8743"), Title = "Test title3" });
-                }
-                _dbContext.SaveChanges();
-                _dbContext.ChangeTracker.Clear();
+                _dbContext.Goals.Add(new GoalEntity() { Id = new Guid("a27723d9-fd4c-4b83-add8-f1c9152585eb"), AssigneeId = new Guid(DefaultMemberVariables.Id), ReporterId = new Guid(DefaultMemberVariables.Id), LabelId = _labelId, DateOfCreate = DateTime.UtcNow, Description = "Test desc2", Title = "Test title2" });
             }
-            _dbContext.ChangeTracker.Clear();
+            var goal3 = _dbContext.Goals.FirstOrDefault(x => x.Id == new Guid("a27723d9-fd4c-4b83-add8-f1c9152585ec"));
+            if (goal3 == null)
+            {
+                _dbContext.Goals.Add(new GoalEntity() { Id = new Guid("a27723d9-fd4c-4b83-add8-f1c9152585ec"), AssigneeId = new Guid(DefaultMemberVariables.Id), ReporterId = new Guid(DefaultMemberVariables.Id), LabelId = _labelId, DateOfCreate = DateTime.UtcNow, Description = "Test desc3", Title = "Test title3" });
+            }
+            
+            var label1 = _dbContext.Labels.FirstOrDefault(x => x.Id == new Guid("4674f93c-6331-4e63-b298-349619fa8741"));
+            if (label1 == null)
+            {
+                _dbContext.Labels.Add(new LabelEntity() { Id = new Guid("4674f93c-6331-4e63-b298-349619fa8741"), Title = "Test title1" });
+            }
+            var label2 = _dbContext.Labels.FirstOrDefault(x => x.Id == new Guid("4674f93c-6331-4e63-b298-349619fa8742"));
+            if (label2 == null)
+            {
+                _dbContext.Labels.Add(new LabelEntity() { Id = new Guid("4674f93c-6331-4e63-b298-349619fa8742"), Title = "Test title2" });
+            }
+            var label3 = _dbContext.Labels.FirstOrDefault(x => x.Id == new Guid("4674f93c-6331-4e63-b298-349619fa8743"));
+            if (label3 == null)
+            {
+                _dbContext.Labels.Add(new LabelEntity() { Id = new Guid("4674f93c-6331-4e63-b298-349619fa8743"), Title = "Test title3" });
+            }
+            _dbContext.SaveChanges();
         }
     }
 }
